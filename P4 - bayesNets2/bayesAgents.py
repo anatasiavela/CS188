8b53f4d7ecb75212e7f9b40734efa2a73adb8f1f
anatasiavela <anastasiavela@berkeley.edu>
2018-04-02 20:25:55 -0700

    print

--- P4 - bayesNets2/bayesAgents.py
+++ P4 - bayesNets2/bayesAgents.py
@@ -213,6 +213,8 @@ def fillObsCPT(bayesNet, gameState):
 
             obsFactor = bn.Factor([obsPos], [FOOD_HOUSE_VAR, GHOST_HOUSE_VAR, X_POS_VAR, Y_POS_VAR], bayesNet.variableDomainsDict())
 
+            print bayesNet.variableDomainsDict()
+
             for assignment in obsFactor.getAllPossibleAssignmentDicts():
                 BLUE_OBS_VAR = 0
                 RED_OBS_VAR = 0
